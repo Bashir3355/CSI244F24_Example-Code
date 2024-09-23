507b5a620fd50d0ff776bc50aead7520c5fcdb0f
Bashir3355 <115440057+Bashir3355@users.noreply.github.com>
2024-09-23 13:21:06 -0700

    added 9-17
    just practice

---
+++ 1variables.js
@@ -0,0 +1,21 @@
+//variables 
+//Dont user var
+var badway = "variable made with var"
+// we use let
+//let is block scope {}
+let newway = "variable made with let";
+//const - variable cannot be changed
+const CONST_VALUE ="I cannot be r-assignment";
+//const_value = 10;
+
+//data Types
+//string
+//number
+//boolean (true or false)
+//Array []
+//object {}
+//underfined
+let numbers = [1, 2, 3,];
+console.log(numbers);
+number[1] = "Hello";
+console.log(numbers);
